95637124525deec3d5934a5a4e304cdc31aac018
kevinmora47 <kevinmora47@gmail.com>
2021-01-13 16:54:10 -0600

    Dom maniputalion

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 
 // Dom manipulation.
 console.log(document.querySelector(".message").textContent);
+// Changing text content
+document.querySelector(".message").textContent = "Kevin 🏋️‍♀️";
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 20;
+
+// Obtaining information.
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
