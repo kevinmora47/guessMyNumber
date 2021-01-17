84fb731ead58edfbb725a3d94683da9206b4e135
kevinmora47 <kevinmora47@gmail.com>
2021-01-16 21:58:10 -0600

    Selecting and maniplating elements

--- index.js
+++ index.js
@@ -4,9 +4,13 @@
 console.log(document.querySelector(".message").textContent);
 // Changing text content
 document.querySelector(".message").textContent = "Kevin 🏋️‍♀️";
-document.querySelector(".number").textContent = 13;
+document.querySelector(".number").textContent = 23;
 document.querySelector(".score").textContent = 20;
 
 // Obtaining information.
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+
+document.querySelector(".message").textContent = "Correct Number! 🎉";
+
+document.querySelector(".number").textContent = 13;
